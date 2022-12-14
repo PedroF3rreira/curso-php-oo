477aabdcc41d9cc6220e430dd566d0b8e1e9457e
Pedro Daniel <pedro-edu19@outlook.com>
2022-12-14 11:41:18 -0300

    adicionado sistema micro template para lista

--- do-estruturado-ao-poo/nivel3/index.php
+++ do-estruturado-ao-poo/nivel3/index.php
@@ -1,82 +1,51 @@
-<?php session_start(); ?>
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" type="text/css" href="css/style.css">
-	<title>listagem de pessoas</title>
-</head>
-<body>
-	<div>
-		<p>
-			<?php
-				if(!empty($_SESSION['flash']))
-				{
-					echo $_SESSION['flash'];
-					$_SESSION['flash'] = '';
-				}
-		  	?>
-		  	
-		  </p>
-	</div>
-	<table >
-		<thead>
-			<th></th>
-			<th></th>
-			<th>Id</th>
-			<th>Nome</th>
-			<th>Endereço</th>
-			<th>Bairro</th>
-			<th>Cidade</th>
-			<th>Telefone</th>
-			<th>Email</th>
-		</thead>
-		<tbody>
-			<?php 
+<?php 
 
-				$conn = mysqli_connect('localhost', 'root', '', 'livro');
+	$conn = mysqli_connect('localhost', 'root', '', 'livro');
 
-				$sql = "SELECT *, pessoas.nome as pessoa, pessoas.id as id_pessoa FROM pessoas INNER JOIN cidades ON pessoas.id_cidade=cidades.id";
+	$sql = "SELECT *, 
+		pessoas.nome as nome, 
+		pessoas.id as id_pessoa, cidades.nome as cidade 
+	FROM pessoas INNER JOIN cidades ON pessoas.id_cidade=cidades.id";
 
-				$result = mysqli_query($conn, $sql);
+	$result = mysqli_query($conn, $sql);
 
-				if(!empty($_GET['action']) && ($_GET['action'] == 'delete') )
-				{
-					$id = (int) $_GET['id'];
+	if(!empty($_GET['action']) && ($_GET['action'] == 'delete') )
+	{
+		$id = (int) $_GET['id'];
 
-					$sql = "DELETE FROM pessoas WHERE id = $id";
-					$success = mysqli_query($conn, $sql);
-					
-					if($success)
-					{
-						$_SESSION['flash'] = 'Registro deletado com suscesso';
-						header('location:index.php');
-					}
+		$sql = "DELETE FROM pessoas WHERE id = $id";
+		$success = mysqli_query($conn, $sql);
+		
+		if($success)
+		{
+			$_SESSION['flash'] = 'Registro deletado com suscesso';
+			header('location:index.php');
+		}
 
-					
-				}
+		
+	}
+	
+	$items = "";
 
-				while($row = mysqli_fetch_assoc($result)){
-					echo "
-						<tr>
-							<td><a href='pessoa_form.php?action=edit&id={$row['id_pessoa']}'>editar</a></td>
-							<td> <a href='index.php?action=delete&id={$row['id_pessoa']}'>excluir</a></td>
-							<td>{$row['id_pessoa']}</td>
-							<td>{$row['pessoa']}</td>
-							<td>{$row['endereco']}</td>
-							<td>{$row['bairro']}</td>
-							<td>{$row['nome']}</td>
-							<td>{$row['telefone']}</td>
-							<td>{$row['email']}</td>
-						</tr>
-					";
-				}
+	while($row = mysqli_fetch_assoc($result)){
+		
+		$item = file_get_contents('html/item.html');
+		$item = str_replace('{id}', $row['id_pessoa'], $item);
+		$item = str_replace('{nome}', $row['nome'], $item);
+		$item = str_replace('{endereco}', $row['endereco'], $item);
+		$item = str_replace('{bairro}', $row['bairro'], $item);
+		$item = str_replace('{telefone}', $row['telefone'], $item);
+		$item = str_replace('{email}', $row['email'], $item);
+		$item = str_replace('{cidade}', $row['cidade'], $item);		
+		
+		$items .= $item;
+	}
+	
+	$list = file_get_contents('html/list.html');
+	$list = str_replace('{items}', $items, $list);
 
-				mysqli_close($conn);
-			?>
-		</tbody>
-	</table>
-	<a href="pessoa_form.php" title="adicionar pessoa">Adicionar</a>
-</body>
-</html>
+	echo $list;
+	
+	
+	mysqli_close($conn);
+?>
--- do-estruturado-ao-poo/nivel3/pessoa_form.php
+++ do-estruturado-ao-poo/nivel3/pessoa_form.php
@@ -1,19 +1,21 @@
 <?php 
+	$pessoa = [];
 	
-	$id 		= '';
-	$nome 		= '';
-	$endereco 	= '';
-	$bairro 	= '';
-	$telefone 	= '';
-	$email 		= '';
-	$id_cidade  = '';
+	$pessoa['id'] 			= '';
+	$pessoa['nome'] 		= '';
+	$pessoa['endereco'] 	= '';
+	$pessoa['bairro'] 		= '';
+	$pessoa['telefone'] 	= '';
+	$pessoa['email'] 		= '';
+	$pessoa['id_cidade']  	= '';
 
 	$conn = mysqli_connect('localhost', 'root', '', 'livro');
+	
 	if(!empty($_REQUEST['action']))
 	{
 		if( $_REQUEST['action'] == 'edit')
 		{
-			if(!empty($_GET['id']))
+			if(!empty($_GET['id']) && is_numeric($_GET['id']))
 			{
 				$result = mysqli_query($conn, 
 					"SELECT pessoas.*, cidades.nome as nome_cidade FROM pessoas 
@@ -22,36 +24,31 @@
 
 				$row = mysqli_fetch_assoc($result);
 
-				$id 		= 	$row['id'];
-				$nome 		= 	$row['nome'];
-				$endereco 	= 	$row['endereco'];
-				$bairro 	=	$row['bairro'];
-				$telefone 	= 	$row['telefone'];
-				$email 		= 	$row['email'];
-				$id_cidade  = 	$row['id_cidade'];
+				$pessoa['id'] 			= 	$row['id'];
+				$pessoa['nome'] 		= 	$row['nome'];
+				$pessoa['endereco'] 	= 	$row['endereco'];
+				$pessoa['bairro'] 		=	$row['bairro'];
+				$pessoa['telefone'] 	= 	$row['telefone'];
+				$pessoa['email'] 		= 	$row['email'];
+				$pessoa['id_cidade'] 	= 	$row['id_cidade'];
 
 			}
 		}
 		else if($_REQUEST['action'] == 'save')
 		{
-			$id 		= $_POST['id'];
-			$nome 		= $_POST['nome'];
-			$endereco 	= $_POST['endereco'];
-			$bairro 	= $_POST['bairro'];
-			$telefone 	= $_POST['telefone'];
-			$email 		= $_POST['email'];
-			$id_cidade 	= $_POST['id_cidade'];
+			$pessoa['id'] 	= $_POST['id'];
+			$pessoa 		= $_POST; 
 
-			if(!empty($id))
+			if(!empty($pessoa['id']))
 			{
 				$result = mysqli_query($conn, "UPDATE pessoas SET 
-					nome 		= '$nome',
-					endereco 	= '$endereco',
-					bairro 		= '$bairro',
-					telefone 	= '$telefone',
-					email 		= '$email',
-					id_cidade 	= '$id_cidade'
-					WHERE id = $id");
+					nome 		= '{$pessoa['nome']}',
+					endereco 	= '{$pessoa['endereco']}',
+					bairro 		= '{$pessoa['bairro']}',
+					telefone 	= '{$pessoa['telefone']}',
+					email 		= '{$pessoa['email']}',
+					id_cidade 	= '{$pessoa['id_cidade']}'
+					WHERE id = '{$pessoa['id']}'");
 
 				if($result)
 				{
@@ -61,7 +58,7 @@
 			else
 			{
 				$sql = "INSERT INTO pessoas (nome, endereco, bairro, telefone, email, id_cidade)
-				VALUES ('{$nome}', '{$endereco}', '{$bairro}', '{$telefone}', '{$email}', '{$id_cidade}')";
+				VALUES ('{$pessoa['nome']}', '{$pessoa['endereco']}', '{$pessoa['bairro']}', '{$pessoa['telefone']}', '{$pessoa['email']}', '{$pessoa['id_cidade']}')";
 
 				$result = mysqli_query($conn, $sql);
 
@@ -75,16 +72,16 @@
 	}
 
 require_once 'lista_combo_cidades.php';
-$cidades = lista_combo_cidades($id_cidade);
+$cidades = lista_combo_cidades($pessoa['id_cidade']);
 
 $form = file_get_contents('html/form.html');
-$form = str_replace('{id}', $id, $form);
-$form = str_replace('{nome}', $nome, $form);
-$form = str_replace('{bairro}', $bairro, $form);
-$form = str_replace('{endereco}', $endereco, $form);
-$form = str_replace('{telefone}', $telefone, $form);
-$form = str_replace('{email}', $email, $form);
-$form = str_replace('{id_cidade}', $id_cidade, $form);
+$form = str_replace('{id}', $pessoa['id'], $form);
+$form = str_replace('{nome}', $pessoa['nome'], $form);
+$form = str_replace('{bairro}', $pessoa['bairro'], $form);
+$form = str_replace('{endereco}', $pessoa['endereco'], $form);
+$form = str_replace('{telefone}', $pessoa['telefone'], $form);
+$form = str_replace('{email}', $pessoa['email'], $form);
+$form = str_replace('{id_cidade}', $pessoa['id_cidade'], $form);
 $form = str_replace('{cidades}', $cidades, $form);
 
 echo $form;
