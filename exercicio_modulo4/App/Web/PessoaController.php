2c43cf728890bee308ad5996d1427472d62075b1
Pedro Daniel <pedro-edu19@outlook.com>
2022-12-29 13:14:30 -0300

    adicionado o twig

--- exercicio_modulo4/App/Web/PessoaController.php
+++ exercicio_modulo4/App/Web/PessoaController.php
@@ -2,12 +2,25 @@
 namespace App\Web;
 
 use App\Models\Pessoa;
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
 
 class PessoaController
 {
-	public function listPessoa()
+	private $twig;
+
+	public function __construct()
 	{
-		var_dump((new Pessoa)->load());
+		$loader = new FilesystemLoader(__DIR__ . '../../templates');
+		$this->twig = new Environment($loader);
+	}
+	public function listPessoa()
+	{	
+		$pessoas = (new Pessoa)->load();
+
+		echo $this->twig->render('list.html.twig',[
+				'pessoas' => $pessoas
+		]);
 	}
 }
 
