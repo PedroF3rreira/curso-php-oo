b83d375f557e7acb22c24fd8e9ed08c261fc14df
Pedro Daniel <pedro-edu19@outlook.com>
2023-01-02 10:52:50 -0300

    active record terminado

--- Percistencia/ActiveRecord/classes/Produto.php
+++ Percistencia/ActiveRecord/classes/Produto.php
@@ -1,5 +1,10 @@
 <?php 
-
+/**
+ * classe padão active record
+ * pros: com apenas um arquivo somos capazes de realizar um crud nas entidades do sistema
+ * contras: feri o principio de responsabilidade unico do S.O.L.I a classe e responsavel por
+ * representar um entidade e suas regras de negocio e tambêm por acessar banco de dados
+ * **/
 class Produto
 {
 	private array $data;
@@ -42,12 +47,85 @@ class Produto
 		return $prepare->fetchAll(PDO::FETCH_CLASS, __CLASS__);
 	}
 
-	public function find($id)
+	public static function find($id)
 	{
 		$sql = "SELECT * FROM produtos WHERE id = :id";
-		$result = self::prepare($sql);
-		$result->execute(['id' = $id]);
+		$result = self::$conn->prepare($sql);
+		$result->execute(['id' => $id]);
 
 		return $result->fetchObject(__CLASS__);
+	}
+
+	public function save()
+	{	
+		if(!$this->id)
+		{
+			$sql = "INSERT INTO produtos 
+			(	
+				descricao, 
+				estoque, 
+				preco_custo, 
+				preco_venda, 
+				codigo_barras, 
+				data_cadastro, 
+				origem
+			) 
+			VALUES
+			(	
+				:descricao, 
+				:estoque, 
+				:preco_custo, 
+				:preco_venda, 
+				:codigo_barras, 
+				:data_cadastro, 
+				:origem
+			)";
+
+			print $sql."<br>";
+			$result = self::$conn->prepare($sql);
+			
+			return $result->execute([
+				'descricao' => $this->descricao, 
+				'estoque' => $this->estoque, 
+				'preco_custo' => $this->preco_custo, 
+				'preco_venda' => $this->preco_venda, 
+				'codigo_barras' => $this->codigo_barras, 
+				'data_cadastro' => $this->data_cadastro, 
+				'origem' => $this->origem
+			]);
+		}
+		else
+		{
+			echo $this->id;
+			$sql = "UPDATE produtos SET 
+				descricao 		= :descricao, 
+				estoque 		= :estoque, 
+				preco_custo		= :preco_custo, 
+				preco_venda 	= :preco_venda, 
+				codigo_barras 	= :codigo_barras, 
+				data_cadastro 	= :data_cadastro, 
+				origem 			= :origem
+				WHERE id = $this->id";
+
+				print $sql."<br>";
+				$result = self::$conn->prepare($sql);
+
+				return $result->execute([
+				'descricao' 	=> $this->descricao, 
+				'estoque' 		=> $this->estoque, 
+				'preco_custo' 	=> $this->preco_custo, 
+				'preco_venda' 	=> $this->preco_venda, 
+				'codigo_barras' => $this->codigo_barras, 
+				'data_cadastro' => $this->data_cadastro, 
+				'origem' 		=> $this->origem
+			]);
+
+		}
+	}
+
+	public function delete()
+	{
+		$result = self::$conn->prepare("DELETE FROM produtos WHERE id = :id");
+		return $result->execute(['id' => $this->id]);
 	}
 }
--- Percistencia/ActiveRecord/teste1.php
+++ Percistencia/ActiveRecord/teste1.php
@@ -4,3 +4,16 @@ require_once 'classes/Connection.php';
 
 Produto::setConnection(Connection::getInstance());
 var_dump(Produto::all('celular'));
+$p1 = new Produto;
+$p1->descricao = 'garrafa termica';
+$p1->estoque = 20;
+$p1->preco_custo = 15.69;
+$p1->preco_venda = 25.69;
+$p1->codigo_barras = '151665165165165165165165165156';
+$p1->data_cadastro = date('Y-m-d');
+$p1->origem = 'N';
+//$p1->save();
+
+$p2 = Produto::find(1);
+var_dump($p2);
+//$p2->delete();
