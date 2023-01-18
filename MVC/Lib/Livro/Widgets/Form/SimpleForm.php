367f74ae0d257be11097f6e88de9e8e687e009db
Pedro Daniel <pedro-edu19@outlook.com>
2023-01-18 16:01:44 -0300

    implementando components

--- MVC/Lib/Livro/Widgets/Form/SimpleForm.php
+++ MVC/Lib/Livro/Widgets/Form/SimpleForm.php
@@ -5,6 +5,7 @@ class SimpleForm
 {
 	private array $fields;
 	private string $title;
+	private string $action;
 
 	public function __construct()
 	{
@@ -16,7 +17,7 @@ class SimpleForm
 		// code...
 	}
 
-	public function addField($label, $name, $type, $placeholder='', $class='', $id='')
+	public function addField($label, $name, $type, $value, $placeholder='', $class='', $id='')
 	{
 		$this->fields[] = [
 			'label' => $label,
@@ -30,6 +31,22 @@ class SimpleForm
 
 	public function addAction($action)
 	{
-		
+		$this->action = $action;
+	}
+
+	public function show()
+	{
+		echo "
+		<div class='panel panel-default'>
+		  <div class='panel-heading'>Panel Heading</div>
+		  <div class='panel-body'>";
+		  foreach($this->fields as $value)
+		  {
+
+		  }
+		echo"
+		</div>
+		</div>
+		";
 	}
 }
