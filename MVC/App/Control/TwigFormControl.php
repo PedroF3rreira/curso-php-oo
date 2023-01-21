7556b6b3eee9680763719ec6d756ca0fc823fae3
Pedro Daniel <pedro-edu19@outlook.com>
2023-01-21 16:10:08 -0300

    adicionsndo components mtwig

--- MVC/App/Control/TwigFormControl.php
+++ MVC/App/Control/TwigFormControl.php
@@ -1,13 +1,45 @@
 <?php 
 use Livro\Control\BaseControl;
+use Livro\Database\Transaction;
+use Livro\Database\Criteria;
+use Livro\Database\Repository;
 
 class TwigFormControl extends BaseControl
 {
+	private $twig;
+
 	public function __construct()
 	{
-		$loader = new \Twig\Loader\FilesystemLoader('App/Resource');
-		$twig = new \Twig\Environment($loader);
+		parent::__construct();
 
-		echo $twig->render('forms/form.html', ['title' => 'teste Twig']);
+		$loader = new \Twig\Loader\FilesystemLoader('App/Resource');
+		$this->twig = new \Twig\Environment($loader);
+	}
+
+	public function index()
+	{
+		try
+		{
+			Transaction::open('livro');
+
+			$criteria = new Criteria();
+			$criteria->setProperty('order', 'id');
+			$repository = new Repository('Pessoa');
+			$pessoas = $repository->load($criteria);
+			
+			Transaction::close();
+
+			echo $this->twig->render('index.html.twig', 
+				[
+					'title' => 'teste Twig',
+					'pessoas' => $pessoas,
+					'columns' => array_keys($pessoas[0]->toArray())
+				]);
+		}
+		catch(Exception $e)
+		{
+			Transaction::rollback();
+			echo $e->getMessage();
+		}
 	}
 }
