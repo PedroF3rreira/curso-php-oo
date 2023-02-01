909589a39e5a94a27f3370b5c594b1d26f4eb64f
Pedro Daniel <pedro-edu19@outlook.com>
2023-02-01 11:46:31 -0300

    a

--- MVC/App/Control/ContatoForm.php
+++ MVC/App/Control/ContatoForm.php
@@ -6,6 +6,7 @@ use Livro\Widgets\Form\Form;
 use Livro\Widgets\Wrapper\BootstrapFormWrapper;
 use Livro\Widgets\Form\Entry;
 use Livro\Widgets\Form\Combo;
+use Livro\Widgets\Form\Text;
 use Livro\Widgets\Dialog\Message;
 
 class ContatoForm extends BaseControl
@@ -13,6 +14,9 @@ class ContatoForm extends BaseControl
 
 	private $form;
 
+	/**
+	 * cria e exibe o formulário na tela 
+	 * **/
 	public function __construct()
 	{
 		parent::__construct();
@@ -23,6 +27,7 @@ class ContatoForm extends BaseControl
 		
 		$nome = new Entry('nome');
 		$email = new Entry('email');
+		$conteudo = new Text('conteudo');
 		
 		$assunto = new Combo('assunto');
 		$assunto->addItems([
@@ -34,13 +39,20 @@ class ContatoForm extends BaseControl
 
 		$this->form->addField('Nome:', $nome);
 		$this->form->addField('Email:', $email);
-		$this->form->addField('Assunto:', $assunto, '35%');
+		$this->form->addField('Assunto:', $assunto, '15%');
+		$this->form->addField('Conteúdo:', $conteudo, '600');
 
 		$this->form->addAction( 'Enviar', new Action([$this, 'onSend']) );
+		$this->form->addAction( 'Salvar', new Action([$this, 'onSave']) );
 
 		parent::add($this->form);
 	}
 
+	public function onSave()
+	{
+		
+	}
+	
 	public function onSend($params)
 	{
 		$data = $this->form->getData();
