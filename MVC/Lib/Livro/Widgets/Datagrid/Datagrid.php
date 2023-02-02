0d8829e046deba118cbc8f9e60dacee84ab0cf06
Pedro Daniel <pedro-edu19@outlook.com>
2023-02-02 17:42:22 -0300

    add files

---
+++ MVC/Lib/Livro/Widgets/Datagrid/Datagrid.php
@@ -0,0 +1,55 @@
+<?php 
+namespace Livro\Widgets\Datagrid;
+
+class Datagrid
+{
+	private $columns;
+	private $actions;
+	private $items;
+
+	public function __construct()
+	{
+		$this->columns = [];
+		$this->actions = [];
+		$this->items = [];
+	}
+	public function addColumn(DatagridColumn $object)
+	{
+		$this->columns[] = $object
+	}
+
+	public function addAction($label, ActionInterface $action, $field, $image = null)
+	{
+		$this->actions[] = [
+			'label' => $label,
+			'action' => $action,
+			'field' => $field,
+			'image' => $image
+		]; 
+	}
+
+	public function addItem($item)
+	{
+		$this->items[] = $item;
+	}
+
+	public function getColumns()
+	{
+		return $this->columns;
+	}
+
+	public function getItems()
+	{
+		return $this->items;
+	}
+
+	public function getActions()
+	{
+		return $this->actions;
+	}
+
+	public function clear()
+	{
+		$this->items = [];
+	}
+}
