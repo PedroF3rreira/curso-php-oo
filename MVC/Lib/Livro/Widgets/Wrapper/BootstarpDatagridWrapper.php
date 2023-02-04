819ccc34cc2f760cafcbee41f64cd556c6a5ee0e
Pedro Daniel <pedro-edu19@outlook.com>
2023-02-04 11:32:58 -0300

    adicionando funçoes a datagrid

--- MVC/Lib/Livro/Widgets/Wrapper/BootstarpDatagridWrapper.php
+++ MVC/Lib/Livro/Widgets/Wrapper/BootstarpDatagridWrapper.php
@@ -1,7 +1,36 @@
 <?php 
 namespace Livro\Widgets\Wrapper;
 
+use Livro\Widgets\Datagrid\Datagrid;
+use Livro\Widgets\Base\Element;
+use Livro\Widgets\Container\Card;
+
 class BootstarpDatagridWrapper
 {
+	private $decorated;
+
+	public function __construct(Datagrid $datagrid)
+	{
+		$this->decorated = $datagrid;
+	}
+
+	public function __call($method, $param)
+	{
+		return call_user_func_array([$this->decorated, $method], $param);
+	}
 	
+	/**
+	 * redireciona alterações nas propiedades do objeto
+	 * **/
+	public function __set($prop, $value)
+	{
+		$this->decorated->$prop = $value;
+	}
+
+	public function show()
+	{
+		$table = new Element('table');
+		$table->class = "table table-striped table-hover";
+
+	}
 }
